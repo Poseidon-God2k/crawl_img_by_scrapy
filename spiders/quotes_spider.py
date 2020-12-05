54e731e236f2db1481c0d85a89fedb19f19ad9de
quyen Luc <ancucchocon@gmail.com>
2020-12-06 01:05:42 +0700

    update store img crawl form website

--- spiders/quotes_spider.py
+++ spiders/quotes_spider.py
@@ -1,5 +1,6 @@
 import scrapy
-
+import os
+import requests as rq
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -16,5 +17,17 @@ class QuotesSpider(scrapy.Spider):
         for cart in response.css('a.grid-product__link'):
             dict_img = {"src":cart.css('img').xpath('@src').get(),"title":cart.css('div.grid-product__title::text').get(),"price":cart.css('span.grid-product__price--original::text').get()}
             lst_img.append(dict_img)
+
+        #write list image to folder
+        os.mkdir('store_img')
+
+        for i in lst_img:
+            title = i['title'].replace(' ','_')+'_'+i['price']
+            img_data = rq.get('https:'+i['src']).content
+            with open("store_img/"+title+'.jpg','wb+') as f:
+                f.write(img_data)
+                f.close()
+
+
 
     
